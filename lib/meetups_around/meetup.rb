e2ee7e397a73d9b801914a72f5d708aa23b37697
Seth <itz.saga@gmail.com>
2017-03-25 12:48:30 -0500

    removed heredoc stub

--- lib/meetups_around/meetup.rb
+++ lib/meetups_around/meetup.rb
@@ -7,11 +7,6 @@ class MeetupsAround::Meetup
         @todays_date = 'Saturday, March 25'
         # I should return all the instances of today's meetups.
         puts "Today #{@todays_date} are the following meetups:"
-        # puts <<~HEREDOC
-        # 1. 2:30PM - Developer Launchpad Nashville - Coding Jam - 19 Developers going
-        # 2. 9:00AM - Score Nashville Events, Networking and Workshops - Join us for a "Start your Business" workshop! - 2 Members going
-        # 3. 10:00AM - Paid to Speak Entrepreneurs - How To Get Your Speaking Career Started - 18 Members Going
-        # HEREDOC
 
         meetup_1 = self.new
         meetup_1.time = "2\:30PM"
