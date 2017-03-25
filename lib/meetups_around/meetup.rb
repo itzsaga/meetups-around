2cb51390dfe6b34fbeaece9cedada723fffe52c6
Seth <itz.saga@gmail.com>
2017-03-25 12:44:38 -0500

    fixed escaping & changed some attribute names

--- lib/meetups_around/meetup.rb
+++ lib/meetups_around/meetup.rb
@@ -1,10 +1,10 @@
 class MeetupsAround::Meetup
-attr_accessor :time, :group_name, :event_name, :attending
-attr_reader :todays_date
+    attr_accessor :time, :group, :event, :attending, :meetups
+    attr_reader :todays_date
 
     def self.today
         @meetups = []
-        @todays_date = "Saturday, March 25"
+        @todays_date = 'Saturday, March 25'
         # I should return all the instances of today's meetups.
         puts "Today #{@todays_date} are the following meetups:"
         # puts <<~HEREDOC
@@ -14,23 +14,23 @@ attr_reader :todays_date
         # HEREDOC
 
         meetup_1 = self.new
-        meetup_1.time = 2:30PM
-        meetup_1.group_name = Developer Launchpad Nashville
-        meetup_1.event_name = Coding Jam
-        meetup_1.attending = 19 Developers going
+        meetup_1.time = "2\:30PM"
+        meetup_1.group = 'Developer Launchpad Nashville'
+        meetup_1.event = 'Coding Jam'
+        meetup_1.attending = '19 Developers going'
 
         meetup_2 = self.new
-        meetup_2.time = 9:00AM
-        meetup_2.group_name = Score Nashville Events, Networking and Workshops
-        meetup_2.event_name = Join us for a "Start your Business" workshop!
-        meetup_2.attending = 2 Members going
+        meetup_2.time = "9\:00AM"
+        meetup_2.group = 'Score Nashville Events, Networking and Workshops'
+        meetup_2.event = 'Join us for a "Start your Business" workshop!'
+        meetup_2.attending = '2 Members going'
 
         meetup_3 = self.new
-        meetup_3.time = 10:00AM
-        meetup_3.group_name = Paid to Speak Entrepreneurs
-        meetup_3.event_name = How To Get Your Speaking Career Started
-        meetup_3.attending = 18 Members Going
+        meetup_3.time = "10\:00AM"
+        meetup_3.group = 'Paid to Speak Entrepreneurs'
+        meetup_3.event = 'How To Get Your Speaking Career Started'
+        meetup_3.attending = '18 Members Going'
 
-        @meetups
+        [meetup_1, meetup_2, meetup_3]
     end
 end
